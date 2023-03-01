673ecef3626678096ebd5c0e884240e687024624
Vivek Nair <vivekmanju53@gmail.com>
2023-03-01 23:38:05 +0530

    link references updates

--- inventoryApp/models.py
+++ inventoryApp/models.py
@@ -1,3 +1,29 @@
+from datetime import datetime
+from unicodedata import category
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+    status = models.IntegerField(default=1)
+    date_added = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class Products(models.Model):
+    code = models.CharField(max_length=100)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.TextField()
+    description = models.TextField()
+    price = models.FloatField(default=0)
+    status = models.IntegerField(default=1) 
+    date_added = models.DateTimeField(default=timezone.now) 
+    date_updated = models.DateTimeField(auto_now=True) 
+
+    def __str__(self):
+        return self.code + " - " + self.name
